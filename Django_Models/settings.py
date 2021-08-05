e7f3585424a431769adb660ee2e55fc6262d1151
SohailAQ <sohail.developes@gmail.com>
2021-08-05 21:19:28 +0530

    Database Configuration

--- Django_Models/settings.py
+++ Django_Models/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = 'Django_Models.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'models',
+       'USER': 'postgres',
+       'PASSWORD': 'root',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
+
 
 
 # Password validation
